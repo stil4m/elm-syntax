a11a85507d49338d09d7a1116591ed97090562f3
Mats Stijlaart <m.stijlaart@avisi.nl>
2017-12-04 22:15:28 +0100

    Add ranged to expose list

--- src/Elm/Syntax/Ranged.elm
+++ src/Elm/Syntax/Ranged.elm
@@ -1,7 +1,18 @@
 module Elm.Syntax.Ranged exposing (Ranged)
 
+{-| Ranged Syntax Elements
+
+
+# Types
+
+@docs Ranged
+
+-}
+
 import Elm.Syntax.Range exposing (Range)
 
 
+{-| Tuple wrapping a source element with a Range
+-}
 type alias Ranged a =
     ( Range, a )
