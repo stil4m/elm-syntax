16ef12bd1f234f5982f6912a4504577042de4096
Mats Stijlaart <mstijlaart@gmail.com>
2018-08-25 10:28:33 +0200

    Elm 0.19: Bump Elm/Parser/Patterns.elm

--- src/Elm/Parser/Patterns.elm
+++ src/Elm/Parser/Patterns.elm
@@ -1,55 +1,51 @@
-module Elm.Parser.Patterns exposing (pattern)
+module Elm.Parser.Patterns exposing (ConsumeArgs, composablePattern, listPattern, numberPart, parensPattern, pattern, qualifiedPattern, qualifiedPatternArg, recordPart, tryToCompose, variablePart)
 
-import Combine exposing (Parser, between, lazy, many, maybe, parens, sepBy, sepBy1, string)
-import Combine.Extra as Combine
+import Combine exposing (Parser, between, choice, lazy, many, maybe, or, parens, sepBy, sepBy1, string, succeed)
+import Combine.Num
 import Elm.Parser.Base as Base exposing (variablePointer)
 import Elm.Parser.Layout as Layout
 import Elm.Parser.Numbers
-import Elm.Parser.Ranges exposing (ranged)
+import Elm.Parser.Ranges exposing (ranged, rangedWithCustomStart)
 import Elm.Parser.State exposing (State)
-import Elm.Parser.Tokens exposing (characterLiteral, functionName, stringLiteral)
+import Elm.Parser.Tokens exposing (asToken, characterLiteral, functionName, stringLiteral, typeName)
+import Elm.Syntax.Base exposing (VariablePointer)
 import Elm.Syntax.Pattern exposing (Pattern(..), QualifiedNameRef)
 import Elm.Syntax.Range as Range exposing (Range)
 import Elm.Syntax.Ranged exposing (Ranged)
+import Parser as Core
 
 
 tryToCompose : Ranged Pattern -> Parser State (Ranged Pattern)
 tryToCompose x =
-    lazy
-        (\() ->
-            maybe Layout.layout
-                |> Combine.continueWith
-                    (Combine.choice
-                        [ Combine.string "as"
-                            |> Combine.ignore Layout.layout
-                            |> Combine.continueWith (variablePointer functionName)
-                            |> Combine.map
-                                (\y ->
-                                    ( Range.combine [ Tuple.first x, y.range ]
-                                    , AsPattern x y
-                                    )
-                                )
-                        , Combine.string "::"
-                            |> Combine.ignore (maybe Layout.layout)
-                            |> Combine.continueWith pattern
-                            |> Combine.map
-                                (\y ->
-                                    ( Range.combine [ Tuple.first x, Tuple.first y ]
-                                    , UnConsPattern x y
-                                    )
-                                )
-                        , Combine.succeed x
-                        ]
-                    )
-        )
+    maybe Layout.layout
+        |> Combine.continueWith
+            (Combine.choice
+                [ Combine.fromCore (Core.keyword "as")
+                    |> Combine.ignore Layout.layout
+                    |> Combine.continueWith (variablePointer functionName)
+                    |> Combine.map
+                        (\y ->
+                            ( Range.combine [ Tuple.first x, y.range ]
+                            , AsPattern x y
+                            )
+                        )
+                , Combine.fromCore (Core.symbol "::")
+                    |> Combine.ignore (maybe Layout.layout)
+                    |> Combine.continueWith pattern
+                    |> Combine.map
+                        (\y ->
+                            ( Range.combine [ Tuple.first x, Tuple.first y ]
+                            , UnConsPattern x y
+                            )
+                        )
+                , Combine.succeed x
+                ]
+            )
 
 
 pattern : Parser State (Ranged Pattern)
 pattern =
-    lazy
-        (\() ->
-            composablePattern |> Combine.andThen tryToCompose
-        )
+    composablePattern |> Combine.andThen tryToCompose
 
 
 parensPattern : Parser State (Ranged Pattern)
@@ -99,64 +95,55 @@ type alias ConsumeArgs =
 
 composablePattern : Parser State (Ranged Pattern)
 composablePattern =
-    lazy
-        (\() ->
-            Combine.choice
-                [ variablePart
-                , qualifiedPattern True
-                , ranged (stringLiteral |> Combine.map StringPattern)
-                , ranged (characterLiteral |> Combine.map CharPattern)
-                , ranged numberPart
-                , ranged (Combine.string "()" |> Combine.map (always UnitPattern))
-                , ranged (Combine.string "_" |> Combine.map (always AllPattern))
-                , recordPart
-                , listPattern
-                , parensPattern
-                ]
-        )
+    Combine.choice
+        [ variablePart
+        , qualifiedPattern True
+        , ranged (stringLiteral |> Combine.map StringPattern)
+        , ranged (characterLiteral |> Combine.map CharPattern)
+        , ranged numberPart
+        , ranged (Core.symbol "()" |> Combine.fromCore |> Combine.map (always UnitPattern))
+        , ranged (Core.symbol "_" |> Combine.fromCore |> Combine.map (always AllPattern))
+        , recordPart
+        , listPattern
+        , parensPattern
+        ]
 
 
 qualifiedPatternArg : Parser State (Ranged Pattern)
 qualifiedPatternArg =
-    lazy
-        (\() ->
-            Combine.choice
-                [ variablePart
-                , qualifiedPattern False
-                , ranged (stringLiteral |> Combine.map StringPattern)
-                , ranged (characterLiteral |> Combine.map CharPattern)
-                , ranged numberPart
-                , ranged (Combine.string "()" |> Combine.map (always UnitPattern))
-                , ranged (Combine.string "_" |> Combine.map (always AllPattern))
-                , recordPart
-                , listPattern
-                , parensPattern
-                ]
-        )
+    Combine.choice
+        [ variablePart
+        , qualifiedPattern False
+        , ranged (stringLiteral |> Combine.map StringPattern)
+        , ranged (characterLiteral |> Combine.map CharPattern)
+        , ranged numberPart
+        , ranged (Core.symbol "()" |> Combine.fromCore |> Combine.map (always UnitPattern))
+        , ranged (Core.symbol "_" |> Combine.fromCore |> Combine.map (always AllPattern))
+        , recordPart
+        , listPattern
+        , parensPattern
+        ]
 
 
 qualifiedPattern : ConsumeArgs -> Parser State ( Range, Pattern )
 qualifiedPattern consumeArgs =
-    lazy
-        (\() ->
-            ranged Base.typeIndicator
-                |> Combine.ignore (maybe Layout.layout)
-                |> Combine.andThen
-                    (\( range, ( mod, name ) ) ->
-                        (if consumeArgs then
-                            many (qualifiedPatternArg |> Combine.ignore (maybe Layout.layout))
+    ranged Base.typeIndicator
+        |> Combine.ignore (maybe Layout.layout)
+        |> Combine.andThen
+            (\( range, ( mod, name ) ) ->
+                (if consumeArgs then
+                    many (qualifiedPatternArg |> Combine.ignore (maybe Layout.layout))
 
-                         else
-                            Combine.succeed []
+                 else
+                    Combine.succeed []
+                )
+                    |> Combine.map
+                        (\args ->
+                            ( Range.combine (range :: List.map Tuple.first args)
+                            , NamedPattern (QualifiedNameRef mod name) args
+                            )
                         )
-                            |> Combine.map
-                                (\args ->
-                                    ( Range.combine (range :: List.map Tuple.first args)
-                                    , NamedPattern (QualifiedNameRef mod name) args
-                                    )
-                                )
-                    )
-        )
+            )
 
 
 recordPart : Parser State (Ranged Pattern)
