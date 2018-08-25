378a5d84713ba7d0d2ad4c1a0be1da5dae191b9f
Mats Stijlaart <mstijlaart@gmail.com>
2018-08-25 10:29:26 +0200

    Elm 0.19: Bump Elm/Syntax/Infix.elm

--- src/Elm/Syntax/Infix.elm
+++ src/Elm/Syntax/Infix.elm
@@ -1,7 +1,4 @@
-module Elm.Syntax.Infix exposing
-    ( Infix, InfixDirection(..)
-    , encode, encodeDirection, decode, decodeDirection
-    )
+module Elm.Syntax.Infix exposing (Infix, InfixDirection(..), decode, decodeDirection, encode, encodeDirection)
 
 {-| Infix Syntax
 
@@ -16,17 +13,19 @@ module Elm.Syntax.Infix exposing
 
 -}
 
+import Elm.Syntax.Range as Range
+import Elm.Syntax.Ranged exposing (Ranged)
 import Json.Decode as JD exposing (Decoder)
-import Json.Decode.Extra exposing ((|:))
 import Json.Encode as JE exposing (Value)
 
 
 {-| Type annotation for a infix definition
 -}
 type alias Infix =
-    { direction : InfixDirection
-    , precedence : Int
-    , operator : String
+    { direction : Ranged InfixDirection
+    , precedence : Ranged Int
+    , operator : Ranged String
+    , function : Ranged String
     }
 
 
@@ -35,6 +34,7 @@ type alias Infix =
 type InfixDirection
     = Left
     | Right
+    | Non
 
 
 {-| Encode an infix
@@ -42,9 +42,30 @@ type InfixDirection
 encode : Infix -> Value
 encode inf =
     JE.object
-        [ ( "direction", encodeDirection inf.direction )
-        , ( "precedence", JE.int inf.precedence )
-        , ( "operator", JE.string inf.operator )
+        [ ( "direction"
+          , JE.object
+                [ ( "range", Range.encode <| Tuple.first inf.direction )
+                , ( "value", encodeDirection <| Tuple.second inf.direction )
+                ]
+          )
+        , ( "precedence"
+          , JE.object
+                [ ( "range", Range.encode <| Tuple.first inf.precedence )
+                , ( "value", JE.int <| Tuple.second inf.precedence )
+                ]
+          )
+        , ( "operator"
+          , JE.object
+                [ ( "range", Range.encode <| Tuple.first inf.operator )
+                , ( "value", JE.string <| Tuple.second inf.operator )
+                ]
+          )
+        , ( "function"
+          , JE.object
+                [ ( "range", Range.encode <| Tuple.first inf.function )
+                , ( "value", JE.string <| Tuple.second inf.function )
+                ]
+          )
         ]
 
 
@@ -59,15 +80,26 @@ encodeDirection d =
         Right ->
             JE.string "right"
 
+        Non ->
+            JE.string "non"
+
+
+decodeRanged : Decoder a -> Decoder (Ranged a)
+decodeRanged sub =
+    JD.map2 Tuple.pair
+        (JD.field "range" Range.decode)
+        (JD.field "value" sub)
+
 
 {-| Decode an infix
 -}
 decode : Decoder Infix
 decode =
-    JD.succeed Infix
-        |: JD.field "direction" decodeDirection
-        |: JD.field "precedence" JD.int
-        |: JD.field "operator" JD.string
+    JD.map4 Infix
+        (JD.field "direction" (decodeRanged decodeDirection))
+        (JD.field "precedence" (decodeRanged JD.int))
+        (JD.field "operator" (decodeRanged JD.string))
+        (JD.field "function" (decodeRanged JD.string))
 
 
 {-| Decode a infix direction
@@ -83,6 +115,9 @@ decodeDirection =
 
                     "right" ->
                         JD.succeed Right
+
+                    "non" ->
+                        JD.succeed Non
 
                     _ ->
                         JD.fail "Invlalid direction"
