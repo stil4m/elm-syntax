6c3d92f8ad7ba56deb393d03236dd549ae2f5db8
Phill Sparks <me@phills.me.uk>
2020-06-17 16:58:55 +0100

    Compare the position of two Ranges

--- tests/tests/Elm/Syntax/RangeTests.elm
+++ tests/tests/Elm/Syntax/RangeTests.elm
@@ -1,6 +1,6 @@
 module Elm.Syntax.RangeTests exposing (suite)
 
-import Elm.Syntax.Range as Range
+import Elm.Syntax.Range as Range exposing (Range)
 import Expect
 import Test exposing (Test, describe, test)
 
@@ -9,6 +9,7 @@ suite : Test
 suite =
     describe "Elm.Syntax.Range"
         [ describe "compareLocations" compareLocationsTests
+        , describe "comparePositions" comparePositionsTests
         ]
 
 
@@ -79,6 +80,88 @@ compareLocationsTests =
                         { left | column = 3 }
                 in
                 Range.compareLocations left right
+                    |> Expect.equal GT
+        ]
+    ]
+
+
+comparePositionsTests : List Test
+comparePositionsTests =
+    [ describe "EQ"
+        [ test "when ranges are equal" <|
+            \() ->
+                let
+                    range : Range
+                    range =
+                        { start = { row = 1, column = 2 }
+                        , end = { row = 3, column = 4 }
+                        }
+                in
+                Range.comparePositions range range
+                    |> Expect.equal EQ
+        ]
+    , describe "LT"
+        [ test "when left start < right start" <|
+            \() ->
+                let
+                    left : Range
+                    left =
+                        { start = { row = 1, column = 1 }
+                        , end = { row = 1, column = 2 }
+                        }
+
+                    right : Range
+                    right =
+                        { left | end = { row = 2, column = 2 } }
+                in
+                Range.comparePositions left right
+                    |> Expect.equal LT
+        , test "when left start == right start and left end < right end" <|
+            \() ->
+                let
+                    left : Range
+                    left =
+                        { start = { row = 2, column = 1 }
+                        , end = { row = 2, column = 3 }
+                        }
+
+                    right : Range
+                    right =
+                        { left | end = { row = 2, column = 9 } }
+                in
+                Range.comparePositions left right
+                    |> Expect.equal LT
+        ]
+    , describe "GT"
+        [ test "when left start > right start" <|
+            \() ->
+                let
+                    left : Range
+                    left =
+                        { start = { row = 5, column = 6 }
+                        , end = { row = 5, column = 7 }
+                        }
+
+                    right : Range
+                    right =
+                        { left | start = { row = 5, column = 2 } }
+                in
+                Range.comparePositions left right
+                    |> Expect.equal GT
+        , test "when left start == right start and left end > right end" <|
+            \() ->
+                let
+                    left : Range
+                    left =
+                        { start = { row = 5, column = 2 }
+                        , end = { row = 5, column = 9 }
+                        }
+
+                    right : Range
+                    right =
+                        { left | end = { row = 5, column = 4 } }
+                in
+                Range.comparePositions left right
                     |> Expect.equal GT
         ]
     ]
