7737e121fb48aff17cfc7ecfa02df2bbf1a83a52
Phill Sparks <me@phills.me.uk>
2020-06-17 17:20:52 +0100

    Test Range.combine

--- tests/tests/Elm/Syntax/RangeTests.elm
+++ tests/tests/Elm/Syntax/RangeTests.elm
@@ -8,9 +8,97 @@ import Test exposing (Test, describe, test)
 suite : Test
 suite =
     describe "Elm.Syntax.Range"
-        [ describe "compareLocations" compareLocationsTests
+        [ describe "combine" combineTests
+        , describe "compareLocations" compareLocationsTests
         , describe "comparePositions" comparePositionsTests
         ]
+
+
+combineTests : List Test
+combineTests =
+    [ test "given an empty list is an empty range" <|
+        \() ->
+            Range.combine []
+                |> Expect.equal Range.emptyRange
+    , test "given a singleton list is the same range" <|
+        \() ->
+            let
+                range : Range
+                range =
+                    { start = { row = 1, column = 1 }
+                    , end = { row = 1, column = 9 }
+                    }
+            in
+            Range.combine [ range ]
+                |> Expect.equal range
+    , test "given a list of ranges, where one covers all the others, gives that range" <|
+        \() ->
+            let
+                outerRange : Range
+                outerRange =
+                    { start = { row = 1, column = 1 }
+                    , end = { row = 5, column = 100 }
+                    }
+
+                ranges : List Range
+                ranges =
+                    [ outerRange
+                    , { start = { row = 2, column = 1 }
+                      , end = { row = 2, column = 9 }
+                      }
+                    , { start = { row = 3, column = 5 }
+                      , end = { row = 4, column = 15 }
+                      }
+                    ]
+            in
+            Range.combine ranges
+                |> Expect.equal outerRange
+    , test "given a list of distinct ranges where none overlap, gives a range over all" <|
+        \() ->
+            let
+                outerRange : Range
+                outerRange =
+                    { start = { row = 2, column = 1 }
+                    , end = { row = 10, column = 69 }
+                    }
+
+                ranges : List Range
+                ranges =
+                    [ { start = outerRange.start
+                      , end = { row = 2, column = 21 }
+                      }
+                    , { start = { row = 4, column = 6 }
+                      , end = { row = 4, column = 100 }
+                      }
+                    , { start = { row = 9, column = 5 }
+                      , end = outerRange.end
+                      }
+                    ]
+            in
+            Range.combine ranges
+                |> Expect.equal outerRange
+    , test "given overlapping ranges, gives a range over both" <|
+        \() ->
+            let
+                outerRange : Range
+                outerRange =
+                    { start = { row = 1, column = 23 }
+                    , end = { row = 32, column = 5 }
+                    }
+
+                ranges : List Range
+                ranges =
+                    [ { start = { row = 15, column = 1 }
+                      , end = outerRange.end
+                      }
+                    , { start = outerRange.start
+                      , end = { row = 16, column = 27 }
+                      }
+                    ]
+            in
+            Range.combine ranges
+                |> Expect.equal outerRange
+    ]
 
 
 compareLocationsTests : List Test
