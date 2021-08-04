6d52ac4abb2afa06660dced889406470298ef8be
Jeroen Engels <jfm.engels@gmail.com>
2021-08-04 23:39:37 +0200

    Inline visitors

--- src/Elm/Processing/Documentation.elm
+++ src/Elm/Processing/Documentation.elm
@@ -44,13 +44,22 @@ inspectDeclaration : Node Declaration -> ThingsToChange -> ThingsToChange
 inspectDeclaration declaration context =
     case Node.value declaration of
         FunctionDeclaration function ->
-            onFunction declaration function context
+            addDocumentation
+                (\doc -> FunctionDeclaration { function | documentation = Just doc })
+                declaration
+                context
 
         AliasDeclaration typeAlias ->
-            onTypeAlias declaration typeAlias context
+            addDocumentation
+                (\doc -> AliasDeclaration { typeAlias | documentation = Just doc })
+                declaration
+                context
 
         CustomTypeDeclaration typeDecl ->
-            onType declaration typeDecl context
+            addDocumentation
+                (\doc -> CustomTypeDeclaration { typeDecl | documentation = Just doc })
+                declaration
+                context
 
         PortDeclaration _ ->
             { unattachedComments = context.unattachedComments
@@ -86,41 +95,6 @@ addDocumentation howToUpdate declaration file =
             , remainingComments = file.remainingComments
             , declarations = declaration :: file.declarations
             }
-
-
-onType : Node Declaration -> Type -> ThingsToChange -> ThingsToChange
-onType declaration customType file =
-    addDocumentation
-        (\doc -> CustomTypeDeclaration { customType | documentation = Just doc })
-        declaration
-        file
-
-
-onTypeAlias : Node Declaration -> TypeAlias -> ThingsToChange -> ThingsToChange
-onTypeAlias declaration typeAlias file =
-    addDocumentation
-        (\doc -> AliasDeclaration { typeAlias | documentation = Just doc })
-        declaration
-        file
-
-
-onFunction : Node Declaration -> Function -> ThingsToChange -> ThingsToChange
-onFunction declaration function file =
-    addDocumentation
-        (\doc -> FunctionDeclaration { function | documentation = Just doc })
-        declaration
-        file
-
-
-replaceDeclaration : Node Declaration -> Node Declaration -> Node Declaration
-replaceDeclaration (Node r1 new) (Node r2 old) =
-    Node r2
-        (if r1 == r2 then
-            new
-
-         else
-            old
-        )
 
 
 findDocumentationForRange : Range -> List (Node String) -> List (Node String) -> Maybe ( List (Node String), Node String, List (Node String) )
