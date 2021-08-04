38d6bac13079fd6bf3b71abf6d5b0df1b8411329
Jeroen Engels <jfm.engels@gmail.com>
2021-08-04 11:48:05 +0200

    Bump elm-syntax to 0.8.5

--- src/Elm/Inspector.elm
+++ src/Elm/Inspector.elm
@@ -34,7 +34,8 @@ type alias Config context =
     , onSignature : Order context (Node Signature)
     , onExpression : Order context (Node Expression)
     , onOperatorApplication :
-        Order context
+        Order
+            context
             { operator : String
             , direction : InfixDirection
             , left : Node Expression
