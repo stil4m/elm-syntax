f570b94c3e9f90138d2c2c579567bca039118fde
Jeroen Engels <jfm.engels@gmail.com>
2023-06-23 17:01:52 +0200

    Replace references to the internal RawFile by the public one

--- src/Elm/Parser.elm
+++ src/Elm/Parser.elm
@@ -7,9 +7,10 @@ module Elm.Parser exposing (parse)
 -}
 
 import Combine exposing (Parser, end, withLocation)
-import Elm.Internal.RawFile as RawFile exposing (RawFile)
+import Elm.Internal.RawFile
 import Elm.Parser.File exposing (file)
 import Elm.Parser.State exposing (State, emptyState)
+import Elm.RawFile exposing (RawFile)
 import Elm.Syntax.File exposing (File)
 import Parser exposing (DeadEnd)
 
@@ -25,7 +26,7 @@ parse input =
     -- A single line is added for unfinished ranges produced by `parser-combinators` on the last line.
     case Combine.runParser (withEnd file) emptyState (input ++ "\n") of
         Ok ( _, r ) ->
-            Ok (RawFile.fromFile r)
+            Ok (Elm.Internal.RawFile.fromFile r)
 
         Err s ->
             Err s
