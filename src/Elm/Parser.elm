b4e9353c30233c4b72e7eefdeca994ffc4ea41f2
Jeroen Engels <jfm.engels@gmail.com>
2023-06-23 18:03:22 +0200

    Add parseToFile

--- src/Elm/Parser.elm
+++ src/Elm/Parser.elm
@@ -1,7 +1,11 @@
-module Elm.Parser exposing (parse)
+module Elm.Parser exposing
+    ( parseToFile
+    , parse
+    )
 
 {-|
 
+@docs parseToFile
 @docs parse
 
 -}
@@ -10,6 +14,7 @@ import Combine exposing (Parser, end, withLocation)
 import Elm.Internal.RawFile as InternalRawFile
 import Elm.Parser.File exposing (file)
 import Elm.Parser.State exposing (State, emptyState)
+import Elm.Processing as Processing
 import Elm.RawFile exposing (RawFile)
 import Elm.Syntax.File exposing (File)
 import Parser exposing (DeadEnd)
@@ -23,10 +28,19 @@ To process a `RawFile`, check out the `Processing` module.
 -}
 parse : String -> Result (List DeadEnd) RawFile
 parse input =
+    parseToFile input
+        |> Result.map InternalRawFile.fromFile
+
+
+{-| Parse some text as if it is an Elm source file.
+When parsing fails, the result will contain a list of errors indicating what went wrong (and/or where).
+-}
+parseToFile : String -> Result (List DeadEnd) File
+parseToFile input =
     -- A single line is added for unfinished ranges produced by `parser-combinators` on the last line.
     case Combine.runParser (withEnd file) emptyState (input ++ "\n") of
         Ok ( _, r ) ->
-            Ok (InternalRawFile.fromFile r)
+            Ok (Processing.process Processing.init (InternalRawFile.fromFile r))
 
         Err s ->
             Err s
