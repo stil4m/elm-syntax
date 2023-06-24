d7b55e547ce7de9a0e988acf17621949d2a3a9b0
Jeroen Engels <jfm.engels@gmail.com>
2023-06-24 14:01:02 +0200

    Add test for comments

--- tests/Elm/Parser/FileTests.elm
+++ tests/Elm/Parser/FileTests.elm
@@ -1,11 +1,13 @@
 module Elm.Parser.FileTests exposing (all)
 
 import Elm.Internal.RawFile as InternalRawFile
+import Elm.Parser
 import Elm.Parser.CombineTestUtil exposing (..)
 import Elm.Parser.File as Parser
 import Elm.Parser.Samples as Samples
 import Elm.Parser.State exposing (emptyState)
 import Elm.RawFile as RawFile exposing (RawFile)
+import Elm.Syntax.Node exposing (Node(..))
 import Expect
 import Json.Decode
 import Json.Encode
@@ -60,4 +62,44 @@ all =
                                 Expect.equal parsed roundTrip
                     )
             )
+        , test "Comments ordering" <|
+            \() ->
+                let
+                    input : String
+                    input =
+                        """
+module Foo exposing (..)
+
+{-| Module documentation
+-}
+
+import A
+
+-- 1
+{- 2 -}
+-- 3
+
+{-| Function declaration
+-}
+f =
+    -- 4
+    identity
+
+-- 5
+{- 6 -}
+"""
+                in
+                Elm.Parser.parseToFile input
+                    |> Result.map .comments
+                    |> Expect.equal
+                        (Ok
+                            [ Node { start = { row = 11, column = 1 }, end = { row = 11, column = 5 } } "-- 3"
+                            , Node { start = { row = 10, column = 1 }, end = { row = 10, column = 8 } } "{- 2 -}"
+                            , Node { start = { row = 9, column = 1 }, end = { row = 9, column = 5 } } "-- 1"
+                            , Node { start = { row = 4, column = 1 }, end = { row = 5, column = 3 } } "{-| Module documentation\n-}"
+                            , Node { start = { row = 16, column = 5 }, end = { row = 16, column = 9 } } "-- 4"
+                            , Node { start = { row = 19, column = 1 }, end = { row = 19, column = 5 } } "-- 5"
+                            , Node { start = { row = 20, column = 1 }, end = { row = 20, column = 8 } } "{- 6 -}"
+                            ]
+                        )
         ]
