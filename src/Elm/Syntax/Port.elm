63fcea997136956677c4886c328381ad88eaf448
Jeroen Engels <jfm.engels@gmail.com>
2023-06-25 17:35:23 +0200

    Expose Elm.Syntax.Port module

--- src/Elm/Syntax/Port.elm
+++ src/Elm/Syntax/Port.elm
@@ -1,4 +1,30 @@
-module Elm.Syntax.Port exposing (Port, decoder, encode)
+module Elm.Syntax.Port exposing
+    ( Port
+    , encode, decoder
+    )
+
+{-| This syntax represents ports.
+For example:
+
+    {-| This is a port.
+    -}
+    port send : String -> Cmd msg
+
+    {-| This is another port.
+    -}
+    port scroll : (Move -> msg) -> Sub msg
+
+
+## Types
+
+@docs Port
+
+
+## Serialization
+
+@docs encode, decoder
+
+-}
 
 import Elm.Syntax.Documentation as Documentation exposing (Documentation)
 import Elm.Syntax.Node as Node exposing (Node)
@@ -7,12 +33,17 @@ import Json.Decode as JD exposing (Decoder)
 import Json.Encode as JE exposing (Value)
 
 
+{-| Information associated with a Port.
+A bit meta, but you get the idea. All information that you can define in a type alias is embedded.
+-}
 type alias Port =
     { documentation : Maybe (Node Documentation)
     , signature : Node Signature
     }
 
 
+{-| Encode a `Port` syntax element to JSON.
+-}
 encode : Port -> Value
 encode { documentation, signature } =
     JE.object
@@ -21,6 +52,8 @@ encode { documentation, signature } =
         ]
 
 
+{-| JSON decoder for a `Port` syntax element.
+-}
 decoder : Decoder Port
 decoder =
     JD.map2 Port
