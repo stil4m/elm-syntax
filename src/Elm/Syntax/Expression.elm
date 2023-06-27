d3e8e906b7be6cb676a8d8a4843a5b2a3f8ecd32
Jeroen Engels <jfm.engels@gmail.com>
2023-06-27 22:21:55 +0200

    Rename OperatorApplication to Operation

--- src/Elm/Syntax/Expression.elm
+++ src/Elm/Syntax/Expression.elm
@@ -78,7 +78,7 @@ type alias FunctionImplementation =
 
   - `Unit`: `()`
   - `Application`: `add a b`
-  - `OperatorApplication`: `a + b`
+  - `Operation`: `a + b`
   - `FunctionOrValue`: `add` or `True`
   - `If`: `if a then b else c`
   - `PrefixOperator`: `(+)`
@@ -104,7 +104,7 @@ type alias FunctionImplementation =
 -}
 type Expression
     = Application (Node Expression) (List (Node Expression))
-    | OperatorApplication String InfixDirection (Node Expression) (Node Expression)
+    | Operation String InfixDirection (Node Expression) (Node Expression)
     | FunctionOrValue ModuleName String
     | If (Node Expression) (Node Expression) (Node Expression)
     | PrefixOperator String
@@ -232,7 +232,7 @@ isCase e =
 isOperatorApplication : Expression -> Bool
 isOperatorApplication e =
     case e of
-        OperatorApplication _ _ _ _ ->
+        Operation _ _ _ _ ->
             True
 
         _ ->
