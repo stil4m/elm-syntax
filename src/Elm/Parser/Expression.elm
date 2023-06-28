7c38155e0fa209fe516a7549a2c9ec06006306d3
Jeroen Engels <jfm.engels@gmail.com>
2023-06-28 08:58:03 +0200

    Rename RecordUpdateExpression to RecordUpdate

--- src/Elm/Parser/Expression.elm
+++ src/Elm/Parser/Expression.elm
@@ -244,7 +244,7 @@ recordContentsCurlyEnd =
                 , syntax =
                     case afterNameBeforeFields.syntax of
                         RecordUpdateFirstSetter firstField ->
-                            RecordUpdateExpression nameNode firstField tailFields.syntax
+                            RecordUpdate nameNode firstField tailFields.syntax
 
                         FieldsFirstValue firstFieldValue ->
                             RecordExpr (Node.combine Tuple.pair nameNode firstFieldValue :: tailFields.syntax)
