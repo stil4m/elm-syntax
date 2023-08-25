9da768e7a32f3f787164c50f92bae2849543a485
Jeroen Engels <jfm.engels@gmail.com>
2023-08-25 14:59:05 +0200

    Remove redundant tests

--- tests/Elm/Parser/LetExpressionTests.elm
+++ tests/Elm/Parser/LetExpressionTests.elm
@@ -56,35 +56,6 @@ all =
                                 }
                             )
                         )
-        , test "let block" <|
-            \() ->
-                parseFullStringState emptyState "let\n  foo = bar\n  \n  john = doe\n in" Parser.letBlock
-                    |> Expect.equal
-                        (Just
-                            [ Node { start = { row = 2, column = 3 }, end = { row = 2, column = 12 } } <|
-                                LetFunction
-                                    { documentation = Nothing
-                                    , signature = Nothing
-                                    , declaration =
-                                        Node { start = { row = 2, column = 3 }, end = { row = 2, column = 12 } }
-                                            { name = Node { start = { row = 2, column = 3 }, end = { row = 2, column = 6 } } "foo"
-                                            , arguments = []
-                                            , expression = Node { start = { row = 2, column = 9 }, end = { row = 2, column = 12 } } <| FunctionOrValue [] "bar"
-                                            }
-                                    }
-                            , Node { start = { row = 4, column = 3 }, end = { row = 4, column = 13 } } <|
-                                LetFunction
-                                    { documentation = Nothing
-                                    , signature = Nothing
-                                    , declaration =
-                                        Node { start = { row = 4, column = 3 }, end = { row = 4, column = 13 } } <|
-                                            { name = Node { start = { row = 4, column = 3 }, end = { row = 4, column = 7 } } "john"
-                                            , arguments = []
-                                            , expression = Node { start = { row = 4, column = 10 }, end = { row = 4, column = 13 } } <| FunctionOrValue [] "doe"
-                                            }
-                                    }
-                            ]
-                        )
         , test "correct let with indent" <|
             \() ->
                 parseFullStringState emptyState "let\n  bar = 1\n in\n  bar" Parser.expression
@@ -136,34 +107,6 @@ all =
                                 }
                             )
                         )
-        , test "let in list" <|
-            \() ->
-                parseFullStringState emptyState "[\n  let\n    bar = 1\n  in\n    bar\n ]" Parser.expression
-                    |> Maybe.map noRangeExpression
-                    |> Maybe.map Node.value
-                    |> Expect.equal
-                        (Just
-                            (ListExpr
-                                [ Node empty <|
-                                    LetExpression
-                                        { declarations =
-                                            [ Node empty <|
-                                                LetFunction
-                                                    { documentation = Nothing
-                                                    , signature = Nothing
-                                                    , declaration =
-                                                        Node empty
-                                                            { name = Node empty "bar"
-                                                            , arguments = []
-                                                            , expression = Node empty <| Integer 1
-                                                            }
-                                                    }
-                                            ]
-                                        , expression = Node empty <| FunctionOrValue [] "bar"
-                                        }
-                                ]
-                            )
-                        )
         , test "some let" <|
             \() ->
                 parseFullStringState emptyState "let\n    _ = b\n in\n    z" Parser.expression
@@ -208,32 +151,6 @@ all =
                                         )
                                     ]
                                 , expression = Node { start = { row = 1, column = 33 }, end = { row = 1, column = 39 } } (FunctionOrValue [] "indent")
-                                }
-                            )
-                        )
-        , test "let starting after definition" <|
-            \() ->
-                parseFullStringState emptyState "foo = let\n  indent = 1\n in\n indent" (functionName |> Combine.continueWith (string " = ") |> Combine.continueWith Parser.expression)
-                    |> Maybe.map noRangeExpression
-                    |> Maybe.map Node.value
-                    |> Expect.equal
-                        (Just
-                            (LetExpression
-                                { declarations =
-                                    [ Node empty <|
-                                        LetFunction
-                                            { documentation = Nothing
-                                            , signature =
-                                                Nothing
-                                            , declaration =
-                                                Node empty <|
-                                                    { name = Node empty "indent"
-                                                    , arguments = []
-                                                    , expression = Node empty <| Integer 1
-                                                    }
-                                            }
-                                    ]
-                                , expression = Node empty <| FunctionOrValue [] "indent"
                                 }
                             )
                         )
