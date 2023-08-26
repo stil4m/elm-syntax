dc0d1224f01d12c6137b974c691ac04d3138a289
Jeroen Engels <jfm.engels@gmail.com>
2023-08-26 12:29:29 +0200

    Don't expose all

--- tests/Elm/Parser/CombineTestUtil.elm
+++ tests/Elm/Parser/CombineTestUtil.elm
@@ -1,12 +1,13 @@
 module Elm.Parser.CombineTestUtil exposing (parseFullString, parseWithState, pushIndent)
 
-import Combine exposing (..)
+import Combine exposing (Parser)
 import Elm.Parser.State exposing (State, emptyState)
 
 
 pushIndent : Int -> Parser State b -> Parser State b
 pushIndent x p =
-    modifyState (Elm.Parser.State.pushColumn (x + 1)) |> Combine.continueWith p
+    Combine.modifyState (Elm.Parser.State.pushColumn (x + 1))
+        |> Combine.continueWith p
 
 
 parseWithState : String -> Parser State a -> Maybe ( State, a )
