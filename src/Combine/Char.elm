8adbc42f87678ca1d97d9a8a4f24aca7eb73eeef
Jeroen Engels <jfm.engels@gmail.com>
2023-08-27 12:52:08 +0200

    Simplify satisfy function

--- src/Combine/Char.elm
+++ src/Combine/Char.elm
@@ -6,41 +6,36 @@ import Parser as Core
 
 char : Char -> Parser s Char
 char c =
-    satisfy (\c_ -> c_ == c)
-        |> Combine.andThen
-            (\maybeChar ->
-                case maybeChar of
-                    Just c_ ->
-                        Combine.succeed c_
-
-                    Nothing ->
-                        Combine.fail ("expected '" ++ String.fromChar c ++ "'")
-            )
+    satisfy
+        (\c_ -> c_ == c)
+        ("expected '" ++ String.fromChar c ++ "'")
 
 
 anyChar : Parser s Char
 anyChar =
-    satisfy (always True)
-        |> Combine.andThen (Maybe.map Combine.succeed >> Maybe.withDefault (Combine.fail "expected any character"))
+    satisfy
+        (always True)
+        "expected any character"
 
 
 oneOf : List Char -> Parser s Char
 oneOf cs =
-    satisfy (\a -> List.member a cs)
-        |> Combine.andThen (Maybe.map Combine.succeed >> Maybe.withDefault (Combine.fail ("expected one of '" ++ String.fromList cs ++ "'")))
+    satisfy
+        (\a -> List.member a cs)
+        ("expected one of '" ++ String.fromList cs ++ "'")
 
 
-satisfy : (Char -> Bool) -> Parser s (Maybe Char)
-satisfy pred =
+satisfy : (Char -> Bool) -> String -> Parser state Char
+satisfy pred problem =
     Combine.fromCore
         (Core.getChompedString (Core.chompIf pred)
             |> Core.andThen
                 (\s ->
                     case String.toList s of
                         [] ->
-                            Core.succeed Nothing
+                            Core.problem problem
 
                         c :: _ ->
-                            Core.succeed (Just c)
+                            Core.succeed c
                 )
         )
