30b0d42ae9ebb28695744ce3b11352eca6854fdb
Jeroen Engels <jfm.engels@gmail.com>
2023-09-16 23:21:21 +0200

    DEBUG

--- src/Elm/Parser/Expression.elm
+++ src/Elm/Parser/Expression.elm
@@ -428,7 +428,7 @@ caseStatementWithCorrectIndentation config =
         (\s ->
             Combine.withLocation
                 (\l ->
-                    if State.expectedColumn (Debug.log "ok" <| s) == l.column then
+                    if Debug.log "condition" <| State.expectedColumn (Debug.log "ok" <| s) == l.column then
                         caseStatement config
 
                     else
