c2d03bb2a084c312543009fa02a53c6005217746
Jeroen Engels <jfm.engels@gmail.com>
2024-07-02 13:53:03 +0200

    Add regression test for comments at the end of declarations

--- tests/Elm/Parser/ModuleTests.elm
+++ tests/Elm/Parser/ModuleTests.elm
@@ -8,6 +8,7 @@ import Elm.Syntax.Exposing exposing (..)
 import Elm.Syntax.Expression exposing (Expression(..))
 import Elm.Syntax.Module exposing (..)
 import Elm.Syntax.Node exposing (Node(..))
+import Elm.Syntax.Pattern exposing (Pattern(..))
 import Elm.Syntax.TypeAnnotation exposing (TypeAnnotation(..))
 import Expect
 import Test exposing (..)
@@ -420,6 +421,66 @@ a : Int
 b = 2
 """
                     |> expectInvalid
+        , test "trailing comments at the end of declarations" <|
+            \() ->
+                parse """module A exposing (fun1, fun2)
+
+fun1 n =
+  fun2 n
+  + fun2 n  -- a
+
+fun2 n =
+  fun1 n    -- b
+"""
+                    File.file
+                    |> Expect.equal
+                        (Just
+                            { comments = [ Node { start = { row = 5, column = 13 }, end = { row = 5, column = 17 } } "-- a", Node { start = { row = 8, column = 13 }, end = { row = 8, column = 17 } } "-- b" ]
+                            , declarations =
+                                [ Node { start = { row = 3, column = 1 }, end = { row = 5, column = 11 } }
+                                    (FunctionDeclaration
+                                        { declaration =
+                                            Node { start = { row = 3, column = 1 }, end = { row = 5, column = 11 } }
+                                                { arguments = [ Node { start = { row = 3, column = 6 }, end = { row = 3, column = 7 } } (VarPattern "n") ]
+                                                , expression =
+                                                    Node { start = { row = 4, column = 3 }, end = { row = 5, column = 11 } }
+                                                        (Application
+                                                            [ Node { start = { row = 4, column = 3 }, end = { row = 4, column = 7 } } (FunctionOrValue [] "fun2")
+                                                            , Node { start = { row = 4, column = 8 }, end = { row = 4, column = 9 } } (FunctionOrValue [] "n")
+                                                            , Node { start = { row = 5, column = 3 }, end = { row = 5, column = 4 } } (Operator "+")
+                                                            , Node { start = { row = 5, column = 5 }, end = { row = 5, column = 9 } } (FunctionOrValue [] "fun2")
+                                                            , Node { start = { row = 5, column = 10 }, end = { row = 5, column = 11 } } (FunctionOrValue [] "n")
+                                                            ]
+                                                        )
+                                                , name = Node { start = { row = 3, column = 1 }, end = { row = 3, column = 5 } } "fun1"
+                                                }
+                                        , documentation = Nothing
+                                        , signature = Nothing
+                                        }
+                                    )
+                                , Node { start = { row = 7, column = 1 }, end = { row = 8, column = 9 } }
+                                    (FunctionDeclaration
+                                        { declaration =
+                                            Node { start = { row = 7, column = 1 }, end = { row = 8, column = 9 } }
+                                                { arguments = [ Node { start = { row = 7, column = 6 }, end = { row = 7, column = 7 } } (VarPattern "n") ]
+                                                , expression =
+                                                    Node { start = { row = 8, column = 3 }, end = { row = 8, column = 9 } }
+                                                        (Application
+                                                            [ Node { start = { row = 8, column = 3 }, end = { row = 8, column = 7 } } (FunctionOrValue [] "fun1")
+                                                            , Node { start = { row = 8, column = 8 }, end = { row = 8, column = 9 } } (FunctionOrValue [] "n")
+                                                            ]
+                                                        )
+                                                , name = Node { start = { row = 7, column = 1 }, end = { row = 7, column = 5 } } "fun2"
+                                                }
+                                        , documentation = Nothing
+                                        , signature = Nothing
+                                        }
+                                    )
+                                ]
+                            , imports = []
+                            , moduleDefinition = Node { start = { row = 1, column = 1 }, end = { row = 1, column = 31 } } (NormalModule { exposingList = Node { start = { row = 1, column = 10 }, end = { row = 1, column = 31 } } (Explicit [ Node { start = { row = 1, column = 20 }, end = { row = 1, column = 24 } } (FunctionExpose "fun1"), Node { start = { row = 1, column = 26 }, end = { row = 1, column = 30 } } (FunctionExpose "fun2") ]), moduleName = Node { start = { row = 1, column = 8 }, end = { row = 1, column = 9 } } [ "A" ] })
+                            }
+                        )
         ]
 
 
