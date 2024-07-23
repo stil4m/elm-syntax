3d472e93c1e6d67895e9670889ae7d1d6f569efc
lue-bird <lue.the.bird@gmail.com>
2024-07-23 06:30:08 +0200

    replace Combine.symbol by Core.symbol

--- src/Combine.elm
+++ src/Combine.elm
@@ -38,7 +38,6 @@ module Combine exposing
     , sepBy1WithoutReverse
     , succeed
     , succeedLazy
-    , symbol
     , withColumn
     , withState
     , withStateFromCore
@@ -209,12 +208,6 @@ succeedLazy createRes =
         )
 
 
-symbol : String -> Parser state ()
-symbol str =
-    Core.symbol str
-        |> fromCore
-
-
 end : Parser state ()
 end =
     Parser <|
@@ -366,15 +359,13 @@ sepBy sep p =
 
 sepBy1 : String -> Parser state a -> Parser state (List a)
 sepBy1 sep p =
-    succeed cons
-        |> keep p
-        |> keep (many (symbol sep |> continueWith p))
+    map cons p
+        |> keep (many (Core.symbol sep |> continueWithFromCore p))
 
 
 sepBy1WithState : Parser state () -> Parser state a -> Parser state (List a)
 sepBy1WithState sep p =
-    succeed cons
-        |> keep p
+    map cons p
         |> keep (many (sep |> continueWith p))
 
 
