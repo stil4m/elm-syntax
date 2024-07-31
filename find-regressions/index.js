736782a22f85c94ef60f1fbf8f9232d0d30680b6
Jeroen Engels <jfm.engels@gmail.com>
2024-07-31 19:13:35 +0200

    Read file

--- find-regressions/index.js
+++ find-regressions/index.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
 
+const fs = require('node:fs/promises');
 const publishedElm = require('./published/elm.js');
 const currentElm = require('./current/elm.js');
+
+const fileToParse = process.argv[2];
+
+(async function () {
+    console.log(fileToParse);
+    const content = await fs.readFile(fileToParse, 'utf8')
+    console.log(content)
+})()
+
