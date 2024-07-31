da1941c3a9796008bd4571bf49185a1b16ebb607
Jeroen Engels <jfm.engels@gmail.com>
2024-07-31 21:24:32 +0200

    Parse through an app

--- find-regressions/index.js
+++ find-regressions/index.js
@@ -6,8 +6,24 @@ const currentElm = require('./current/elm.js');
 
 const fileToParses = process.argv.slice(2);
 
+console.log(publishedElm);
+const published = publishedElm.Elm.ParseMain.init();
+const current = currentElm.Elm.ParseMain.init();
+
 for (const filePath of fileToParses) {
-    console.log(filePath);
-    const content = fs.readFileSync(filePath, 'utf8')
-    console.log(content)
+    //const publishedName = 'published ' + filePath;
+    //const currentName = 'current ' + filePath;
+    //console.time(publishedName)
+    //console.timeEnd(publishedName)
+    //console.time(currentName)
+    //console.timeEnd(currentName)
+    const content = fs.readFileSync(filePath, 'utf8');
+    const promise1 = new Promise((resolve) => {
+      function fn(data) {
+        published.ports.parseResult.unsubscribe(fn);
+        resolve(data);
+      }
+      published.ports.parseResult.subscribe(fn);
+      published.ports.requestParsing.send(content);
+    });
 }
