d4d5f5c48af7f3b068a0099555012ed4e748079b
Jeroen Engels <jfm.engels@gmail.com>
2024-08-01 16:34:06 +0200

    Run find-regressions in make-and-patch

--- find-regressions/make-and-patch.sh
+++ find-regressions/make-and-patch.sh
@@ -10,3 +10,6 @@ cd find-regressions/published
 elm make --optimize ../src/ParseMain.elm --output elm.js
 sed -i 's/$author$project$ParseMain$timeStart(version)/globalThis.performance.now()/' elm.js
 sed -i 's/$author$project$ParseMain$timeEnd(version)/globalThis.measurements[version].push(globalThis.performance.now() - start)/' elm.js
+
+cd ../..
+node find-regressions $@
