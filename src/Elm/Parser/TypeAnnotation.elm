8d5510b9040065b32d27e79556189fe7e383b3ea
lue-bird <lue.the.bird@gmail.com>
2024-08-03 10:12:18 +0200

    move closing record type } inside oneOf

--- src/Elm/Parser/TypeAnnotation.elm
+++ src/Elm/Parser/TypeAnnotation.elm
@@ -294,9 +294,10 @@ recordTypeAnnotation =
                             , Parser.succeed { comments = Rope.empty, syntax = [] }
                             ]
                     ]
-            , Parser.succeed Nothing
+                |. Tokens.curlyEnd
+            , Parser.map (\() -> Nothing)
+                Tokens.curlyEnd
             ]
-        |. Tokens.curlyEnd
 
 
 typeAnnotationRecordEmpty : TypeAnnotation
