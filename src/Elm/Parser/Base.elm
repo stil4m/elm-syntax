d40b1170cfd8a4108a7f2fd406e2303747ca3d7b
lue-bird <lue.the.bird@gmail.com>
2024-08-05 16:01:50 +0200

    replace last instances of getPosition

--- src/Elm/Parser/Base.elm
+++ src/Elm/Parser/Base.elm
@@ -1,25 +1,18 @@
 module Elm.Parser.Base exposing (moduleName)
 
 import CustomParser
+import Elm.Parser.Node as Node
 import Elm.Parser.Tokens as Tokens
 import Elm.Syntax.ModuleName exposing (ModuleName)
-import Elm.Syntax.Node exposing (Node(..))
+import Elm.Syntax.Node exposing (Node)
 
 
 moduleName : CustomParser.Parser (Node ModuleName)
 moduleName =
-    CustomParser.map4
-        (\start head tail endColumn ->
-            Node
-                { start = start
-                , end = { row = start.row, column = endColumn }
-                }
-                (head :: tail)
-        )
-        CustomParser.getPosition
+    CustomParser.map2 (\head tail -> head :: tail)
         Tokens.typeName
         moduleNameOrEmpty
-        CustomParser.getCol
+        |> Node.parserCore
 
 
 moduleNameOrEmpty : CustomParser.Parser ModuleName
