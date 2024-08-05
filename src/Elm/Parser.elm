5acb6a4352ef71c2677f23aed916dbc63bcea9ba
lue-bird <lue.the.bird@gmail.com>
2024-08-05 02:43:42 +0200

    use DeadEnd, Problem from elm/parser

--- src/Elm/Parser.elm
+++ src/Elm/Parser.elm
@@ -10,11 +10,12 @@ module Elm.Parser exposing
 
 -}
 
-import CustomParser exposing (DeadEnd)
+import CustomParser
 import Elm.Internal.RawFile as InternalRawFile
 import Elm.Parser.File exposing (file)
 import Elm.RawFile exposing (RawFile)
 import Elm.Syntax.File exposing (File)
+import Parser
 
 
 {-| **@deprecated** Use [`parseToFile`](#parseToFile) instead, which is simpler and doesn't require post-processing.
--- tests/Elm/Parser/TestUtil.elm
+++ tests/Elm/Parser/TestUtil.elm
@@ -1,6 +1,7 @@
 module Elm.Parser.TestUtil exposing (parse, parseToResult)
 
 import CustomParser
+import Parser
 
 
 parse : String -> CustomParser.Parser a -> Maybe a
@@ -8,6 +9,6 @@ parse source p =
     parseToResult source p |> Result.toMaybe
 
 
-parseToResult : String -> CustomParser.Parser a -> Result (List CustomParser.DeadEnd) a
+parseToResult : String -> CustomParser.Parser a -> Result (List Parser.DeadEnd) a
 parseToResult source p =
     CustomParser.run (p |> CustomParser.ignore CustomParser.end) source
