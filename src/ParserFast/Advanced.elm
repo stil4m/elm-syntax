375e8dbf9b8084a6cd0af39fd8d445817f444e3a
lue-bird <lue.the.bird@gmail.com>
2024-08-21 13:47:37 +0200

    correct isSubCharSinglePart uses

--- src/ParserFast/Advanced.elm
+++ src/ParserFast/Advanced.elm
@@ -977,7 +977,7 @@ keyword kwd expecting res =
                 newOffset =
                     isSubString kwd kwdLength s.offset s.src
             in
-            if newOffset == -1 || not (isSubCharSinglePart (\c -> Char.Extra.isAlphaNumFast c || c == '_') newOffset s.src) then
+            if newOffset == -1 || isSubCharSinglePart (\c -> Char.Extra.isAlphaNumFast c || c == '_') newOffset s.src then
                 Bad False (fromState s expecting) ()
 
             else
@@ -1009,7 +1009,7 @@ keywordFollowedBy kwd expecting (Parser parseNext) =
                 newOffset =
                     isSubString kwd kwdLength s.offset s.src
             in
-            if newOffset == -1 || not (isSubCharSinglePart (\c -> Char.Extra.isAlphaNumFast c || c == '_') newOffset s.src) then
+            if newOffset == -1 || isSubCharSinglePart (\c -> Char.Extra.isAlphaNumFast c || c == '_') newOffset s.src then
                 Bad False (fromState s expecting) ()
 
             else
