e19c9e43c22016fcf3072a4c157898c51b38767e
lue-bird <lue.the.bird@gmail.com>
2024-09-11 03:04:33 +0200

    correct float handling of the custom parser

--- tests/Elm/Parser/NumbersTests.elm
+++ tests/Elm/Parser/NumbersTests.elm
@@ -32,6 +32,76 @@ all =
                         )
                         |> Expect.equal
                             (Ok 42)
+            , test "float" <|
+                \() ->
+                    parseToResult "2.0"
+                        (ParserFast.floatOrIntegerDecimalOrHexadecimalMapWithRange
+                            (\_ n -> n)
+                            (\_ _ -> -1)
+                            (\_ _ -> -1)
+                        )
+                        |> Expect.equal
+                            (Ok 2.0)
+            , test "integer with negative exponent" <|
+                \() ->
+                    parseToResult "2e-2"
+                        (ParserFast.floatOrIntegerDecimalOrHexadecimalMapWithRange
+                            (\_ n -> n)
+                            (\_ _ -> -1)
+                            (\_ _ -> -1)
+                        )
+                        |> Expect.equal
+                            (Ok 2.0e-2)
+            , test "integer with negative exponent (uppercase E)" <|
+                \() ->
+                    parseToResult "2E-2"
+                        (ParserFast.floatOrIntegerDecimalOrHexadecimalMapWithRange
+                            (\_ n -> n)
+                            (\_ _ -> -1)
+                            (\_ _ -> -1)
+                        )
+                        |> Expect.equal
+                            (Ok 2.0e-2)
+            , test "integer with positive exponent" <|
+                \() ->
+                    parseToResult "2e+2"
+                        (ParserFast.floatOrIntegerDecimalOrHexadecimalMapWithRange
+                            (\_ n -> n)
+                            (\_ _ -> -1)
+                            (\_ _ -> -1)
+                        )
+                        |> Expect.equal
+                            (Ok 2.0e2)
+            , test "float with negative exponent" <|
+                \() ->
+                    parseToResult "2.0e-2"
+                        (ParserFast.floatOrIntegerDecimalOrHexadecimalMapWithRange
+                            (\_ n -> n)
+                            (\_ _ -> -1)
+                            (\_ _ -> -1)
+                        )
+                        |> Expect.equal
+                            (Ok 2.0e-2)
+            , test "float with negative exponent (uppercase E)" <|
+                \() ->
+                    parseToResult "2.0E-2"
+                        (ParserFast.floatOrIntegerDecimalOrHexadecimalMapWithRange
+                            (\_ n -> n)
+                            (\_ _ -> -1)
+                            (\_ _ -> -1)
+                        )
+                        |> Expect.equal
+                            (Ok 2.0e-2)
+            , test "float with positive exponent" <|
+                \() ->
+                    parseToResult "2.0e+2"
+                        (ParserFast.floatOrIntegerDecimalOrHexadecimalMapWithRange
+                            (\_ n -> n)
+                            (\_ _ -> -1)
+                            (\_ _ -> -1)
+                        )
+                        |> Expect.equal
+                            (Ok 2.0e2)
 
             -- TODO handling overflow like elm-format / the elm compiler
             -- would technically be a breaking change and maybe somewhat difficult to implement
