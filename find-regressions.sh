76756fbae24be59fdf3d9074ce36ac8be424296f
Jeroen Engels <jfm.engels@gmail.com>
2024-09-11 22:43:37 +0200

    Use a single sed to make replacements in regression script

--- find-regressions.sh
+++ find-regressions.sh
@@ -21,14 +21,16 @@
 
 cd find-regressions/current
 elm make --optimize ../src/ParseMain.elm --output elm.js > /dev/null
-sed -i 's/$author$project$ParseMain$timeStart(version)/globalThis.performance.now()/' elm.js
-sed -i 's/$author$project$ParseMain$timeEnd(version)/globalThis.measurements[version].push(globalThis.performance.now() - start)/' elm.js
+sed -e 's/$author$project$ParseMain$timeStart(version)/globalThis.performance.now()/' \
+    -e 's/$author$project$ParseMain$timeEnd(version)/globalThis.measurements[version].push(globalThis.performance.now() - start)/' \
+    -i elm.js
 
 cd ../..
 cd find-regressions/published
 elm make --optimize ../src/ParseMain.elm --output elm.js > /dev/null
-sed -i 's/$author$project$ParseMain$timeStart(version)/globalThis.performance.now()/' elm.js
-sed -i 's/$author$project$ParseMain$timeEnd(version)/globalThis.measurements[version].push(globalThis.performance.now() - start)/' elm.js
+sed -e 's/$author$project$ParseMain$timeStart(version)/globalThis.performance.now()/' \
+    -e 's/$author$project$ParseMain$timeEnd(version)/globalThis.measurements[version].push(globalThis.performance.now() - start)/' \
+    -i elm.js
 
 cd ../..
 node find-regressions $@
